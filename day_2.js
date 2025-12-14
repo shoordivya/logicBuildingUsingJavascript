23909a853f5179f6e1a32aeeeb22da3c93850540
Divya Shoor <shoordivya22@gmail.com>
2025-12-14 23:12:35 +0530

    Third Day

--- day_2.js
+++ day_2.js
@@ -1,7 +1,20 @@
 //Create a const object and add a new key to it — does it work?
-const obj = {name: "Divya", age:23};
-obj.hobby="watching Cricket";
-console.log(obj); // { name: 'Divya', age: 23, hobby: 'watching Cricket' } because const prevents reassignment and redeclaration, not mutation
-//Try accessing a let variable before declaring it — what error do you see?
-// console.log(a); // ReferenceError: Cannot access 'a' before initialization
-// let a = 10;
+// const obj = {name: "Divya", age:23};
+// obj.hobby="watching Cricket";
+// console.log(obj); // { name: 'Divya', age: 23, hobby: 'watching Cricket' } because const prevents reassignment and redeclaration, not mutation
+// //Try accessing a let variable before declaring it — what error do you see?
+// // console.log(a); // ReferenceError: Cannot access 'a' before initialization
+// // let a = 10;
+
+// const array = [1,2,3,4,5];
+// array.push(6);
+// console.log(array); // [ 1, 2, 3, 4, 5, 6 ] because const prevents reassignment and redeclaration, not mutation
+// console.log(null + 1);     // Output: 1 
+// console.log("5" + 3);    // Output: 53 (string concatenation)   
+// console.log("5" - 3);     // Output: 2 (string is converted to number)  
+// console.log(true + false); // Output: 1 (true is 1, false is 0)
+// console.log(undefined + 1); // Output: NaN (undefined is not a number)
+
+// console.log(typeof []); //object
+// console.log(typeof null); //object
+// console.log(typeof 123n);  //bigint
---
+++ day_3.js
@@ -0,0 +1,17 @@
+// function isEmpty (val){
+//     if(val === null) return "value is null";
+//     if(val === undefined) return "value is undefined";
+//     if(val === "" ) return "it is blank string";
+//     return val;
+// }
+
+
+// console.log(isEmpty(""));
+// console.log(isEmpty(null));
+// console.log(isEmpty(0));
+// console.log(isEmpty());
+
+console.log("10" + 1);  
+console.log("10" - 1);  
+console.log(true + false);
+console.log(!!"Sheryians");
