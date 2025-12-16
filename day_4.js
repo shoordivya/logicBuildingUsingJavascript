7ced69c1432fb16a18c858084e5b11c48f51a8fe
Divya Shoor <shoordivya22@gmail.com>
2025-12-16 23:59:17 +0530

    Day 4 learning Array

---
+++ day_4.js
@@ -0,0 +1,10 @@
+//Create an array of student names and print each
+// const students = ['Divya','priya','shiva','vina'];
+// // for(let i=0;i<students.length;i++){
+// //     console.log('Name: ',students[i]);
+// // } first way
+// students.forEach(name => {
+//     console.log(`Name : ${name}`);
+// });
+//Filter even numbers from an array
+// const numbers =
